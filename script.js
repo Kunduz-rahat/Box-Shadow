e18d9895a85e43f3a698ab4ad7512303fa25ffc7
Kunduz Rahat <rahatova.kunduz@gmail.com>
2021-08-02 13:53:16 +0300

    add css

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const out = document.querySelector('.out')
 const outset = document.querySelector('.outset')
 const inset = document.querySelector('.inset')
 const horizontalShadow = document.querySelector('.horizontal-shadow')
+const verticalShadow = document.querySelector('.vertical-shadow')
+const blurRadius = document.querySelector('.blur-radius')
+const spreadRadius = document.querySelector('.spread-radius')
+const shadow = document.querySelector('.shadow')
+const border = document.querySelector('.border')
+const background = document.querySelector('.background')
 
 //onchange
 // oninput
@@ -25,6 +31,12 @@ out.oninput = cssGenerator
 outset.oninput = cssGenerator
 inset.oninput = cssGenerator
 horizontalShadow.oninput = cssGenerator
+verticalShadow.oninput = cssGenerator
+blurRadius.oninput = cssGenerator
+spreadRadius.oninput = cssGenerator
+shadow.oninput = cssGenerator
+border.oninput = cssGenerator
+background.oninput = cssGenerator
 
 
 function cssGenerator() {
@@ -36,6 +48,12 @@ function cssGenerator() {
   out.innerHTML = out.innerHTML + "-webkit - box - shadow:" + `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px   rgba ${shadowColor.value} ${shadiwInset};<br>`
   out.innerHTML = out.innerHTML + "-moz-box-shadow:" + `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px   rgba ${shadowColor.value} ${shadiwInset};`
   horizontalShadow.textContent = `${offsetX.value}px`
+  verticalShadow.textContent = `${offsetY.value}px`
+  blurRadius.textContent =`${blur.value}px`
+  spreadRadius.textContent = `${spread.value}px`
+  shadow.textContent = `rgba ${borderColor.value}`
+  border.textContent = borderRadius.value + "%";
+  background.textContent = `rgba ${borderColor.value}`;
 }
 
 cssGenerator()
